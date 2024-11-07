8336a2e621fa064a206d3a406bcf4aa820aef0a5
Your Name <your-email@example.com>
2024-11-06 20:49:40 -0800

    wip

--- Mockup.cpp
+++ Mockup.cpp
@@ -12,12 +12,44 @@
 
 using namespace std;
 
+//Dog structure
 struct Dog {
-
+	string dogID;
+	string dogBreed;
+	int dogAge;
 };
 
-int main() {
+//Prototypes
+ 
+// Function to load data from external file
+void loadData(const string& fileName, map<string, array<list<Dog>, 3>>& invetory);
 
+//// Function to process events for a given time period
+void process_events(map<string, array<list<Dog>, 3>>& inventory, int period);
+
+// Function to display the current state of the inventory
+void display_inventory(const map<string, array<list<Dog>, 3>>& inventory, int period);
+
+int main() {
+	// Initialize dogInventory map
+
+	// Load initial data from external file
+	//loadData("dogs_data.txt", dogInventory);
 
 	return 0;
+}
+
+// Function to load data from external file
+void loadData(const string& fileName, map<string, array<list<Dog>, 3>>& invetory) {
+
+}
+
+//// Function to process events for a given time period
+void process_events(map<string, array<list<Dog>, 3>>& inventory, int period) {
+
+}
+
+// Function to display the current state of the inventory
+void display_inventory(const map<string, array<list<Dog>, 3>>& inventory, int period) {
+
 }
