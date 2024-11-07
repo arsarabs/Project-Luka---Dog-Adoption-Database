8d4d7e099945a354818f190b7342fbd4dd8c074b
Your Name <your-email@example.com>
2024-11-06 20:42:56 -0800

    STARTING. I already have an idea on how I want to tackle this.

---
+++ Mockup.cpp
@@ -0,0 +1,23 @@
+// Include necessary headers
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <list>
+#include <array>
+#include <map>
+#include <string>
+#include <algorithm>
+#include <random>
+#include <chrono>
+
+using namespace std;
+
+struct Dog {
+
+};
+
+int main() {
+
+
+	return 0;
+}
